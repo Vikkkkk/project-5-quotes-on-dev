393e93c6dc069064e5c46b8cadf59f5f0fe49240
Vikkkkk <vicguo.99@gmail.com>
2018-11-21 14:15:05 -0800

    added archive page

--- header.php
+++ header.php
@@ -22,9 +22,11 @@
 
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					
+					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>					
 				</div><!-- .site-branding -->
+				<div class="logo-container">
+					<img src="<?php echo get_template_directory_uri();?>/assets/qod-logo.svg" " alt="qod logo">
+				</div>
 
 				
 			</header><!-- #masthead -->
